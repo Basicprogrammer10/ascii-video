1eb264f2bc7b0cf8da0bccf33ef7e47d4db74032
Connor Slade <connor@connorcode.com>
2022-11-15 01:00:44 -0500

    Ascii video CLI start

--- src/main.rs
+++ src/main.rs
@@ -4,55 +4,116 @@ use std::io::Write;
 use std::fs::File;
 use std::io::BufReader;
 
+use clap::{Arg, Command};
 use rodio::{source::Source, Decoder, OutputStream};
 
-const IMG_CHARS: [char; 10] = ['.', ',', '+', 'o', '^', '*', '0', '&', '#', '@'];
+const IMG_CHARS: [char; 10] = ['.', ',', '+', '^', 'o', '*', '&', '0', '#', '@'];
+// const IMG_CHARS: [char; 4] = ['.', '!', 'o', 'm'];
+// const IMG_CHARS: [char; 4] = ['🐄', '🐼', '🐱', '🐮'];
 
 fn main() {
-    // play(fs::read_to_string("out.txt").unwrap(), 15);
+    // Clap arg parser
+    // Subcommands:
+    // - play: takes in a file to play and an optional audio file
+    // - convert: takes in a folder path (folder of images) and an output file path and if dithering should be used
+    let matches = Command::new("Image to Audio")
+        .version("0.1")
+        .author("Connor Slade <connor@connorcode.com>")
+        .about("converts video to text")
+        .subcommand(
+            Command::new("play")
+                .about("plays back an ascii video")
+                .arg(Arg::new("file").help("the file to play").required(true))
+                .arg(
+                    Arg::new("audio")
+                        .help("the audio file to play")
+                        .required(false),
+                ),
+        )
+        .subcommand(
+            Command::new("convert")
+                .about("converts a folder of images to an ascii video")
+                .arg(
+                    Arg::new("folder")
+                        .help("the folder of images to convert")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("output")
+                        .help("the output file to write to")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("dither")
+                        .help("whether to use dithering")
+                        .required(false),
+                ),
+        )
+        .get_matches();
 
-    let mut out = String::new();
-    let mut frames = fs::read_dir("frames")
-        .unwrap()
-        .map(|x| x.unwrap())
-        .collect::<Vec<_>>();
-
-    frames.sort_by(|x, y| {
-        let x = x.path();
-        let y = y.path();
-        let x = x.to_str().unwrap();
-        let y = y.to_str().unwrap();
-
-        let x_parts = x.split('.').collect::<Vec<&str>>();
-        let y_parts = y.split('.').collect::<Vec<&str>>();
-
-        if x_parts.len() == 3 && y_parts.len() == 3 {
-            return x_parts[1]
-                .parse::<u32>()
-                .unwrap()
-                .cmp(&y_parts[1].parse().unwrap());
+    match matches.subcommand() {
+        Some(("play", matches)) => {
+            let file = matches.get_one::<String>("file").unwrap();
+            let audio = matches.get_one::<String>("audio");
+            // play(file, audio);
         }
-
-        x.cmp(y)
-    });
-
-    for i in frames {
-        println!("Processing `{}`", i.file_name().into_string().unwrap());
-
-        let size = (100, 100);
-
-        let img = image::open(i.path()).unwrap();
-        let img = img
-            .resize(size.0, size.1, image::imageops::FilterType::Triangle)
-            .into_rgb8();
-
-        let frame = asciify(im_load(img));
-        out.push_str(&frame);
-        out.push('\n');
-        out.push('\n');
+        Some(("convert", matches)) => {
+            let folder = matches.get_one::<String>("folder").unwrap();
+            let output = matches.get_one::<String>("output").unwrap();
+            let dither = matches.get_flag("dither");
+            // convert(folder, output, dither);
+        }
+        _ => {}
     }
 
-    fs::write("out.txt", out).unwrap();
+    // play(
+    //     fs::read_to_string("out.txt").unwrap(),
+    //     fs::read("audio.mp3").unwrap(),
+    //     15,
+    // );
+
+    // let mut out = String::new();
+    // let mut frames = fs::read_dir("frames")
+    //     .unwrap()
+    //     .map(|x| x.unwrap())
+    //     .collect::<Vec<_>>();
+    //
+    // frames.sort_by(|x, y| {
+    //     let x = x.path();
+    //     let y = y.path();
+    //     let x = x.to_str().unwrap();
+    //     let y = y.to_str().unwrap();
+    //
+    //     let x_parts = x.split('.').collect::<Vec<&str>>();
+    //     let y_parts = y.split('.').collect::<Vec<&str>>();
+    //
+    //     if x_parts.len() == 3 && y_parts.len() == 3 {
+    //         return x_parts[1]
+    //             .parse::<u32>()
+    //             .unwrap()
+    //             .cmp(&y_parts[1].parse().unwrap());
+    //     }
+    //
+    //     x.cmp(y)
+    // });
+    //
+    // for i in frames {
+    //     println!("Processing `{}`", i.file_name().into_string().unwrap());
+    //
+    //     let size = (200, 200);
+    //
+    //     let img = image::open(i.path()).unwrap();
+    //     let img = img
+    //         .resize(size.0, size.1, image::imageops::FilterType::Triangle)
+    //         .into_rgb8();
+    //
+    //     let frame = asciify(im_load(img));
+    //     out.push_str(&frame);
+    //     out.push('\n');
+    //     out.push('\n');
+    // }
+    //
+    // fs::write("out.txt", out).unwrap();
 }
 
 fn im_load(img: image::RgbImage) -> Vec<Vec<f32>> {
@@ -64,7 +125,7 @@ fn im_load(img: image::RgbImage) -> Vec<Vec<f32>> {
             let px = img.get_pixel(x, y).0;
             let px = px[0] as u16 + px[1] as u16 + px[2] as u16;
             let per = (px / 3) as f32 / 255.0;
-            assert!(per < 1.0);
+            assert!(per <= 1.0);
             v.push(per);
         }
         image.push(v);
@@ -79,12 +140,11 @@ fn asciify(mut image: Vec<Vec<f32>>) -> String {
     for y in 0..dim.1 as usize {
         for x in 0..dim.0 as usize {
             let mut px = image[y][x];
-            // todo fix this
             if px > 1.0 {
-                px = 0.99;
+                px = 1.0;
             }
 
-            let index = (px * IMG_CHARS.len() as f32).floor();
+            let index = (px * (IMG_CHARS.len() - 1) as f32).floor();
             let chr = IMG_CHARS[index as usize];
             let err = px - index / IMG_CHARS.len() as f32;
 
@@ -104,22 +164,22 @@ fn asciify(mut image: Vec<Vec<f32>>) -> String {
     out
 }
 
-fn play(data: String, fps: u16) {
-    let fpms = 1000 / fps as u128;
+fn play(data: String, audio: Vec<u8>, fps: u16) {
+    let fpms = 1000.0 / fps as f32;
     let data = data.replace("\r", "   ");
     let frames = data.split("\n\n");
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open("audio.mp3").unwrap());
+    let file = std::io::Cursor::new(audio);
     let source = Decoder::new(file).unwrap();
     stream_handle.play_raw(source.convert_samples()).unwrap();
 
-    for i in frames {
+    for i in frames.skip(15) {
         let start = std::time::Instant::now();
         std::io::stdout().write_all("\x1B[H".as_bytes()).unwrap();
         std::io::stdout().write_all(i.as_bytes()).unwrap();
         std::io::stdout().flush().unwrap();
 
-        while start.elapsed().as_millis() < fpms {}
+        while (start.elapsed().as_millis() as f32) < fpms {}
     }
 }
